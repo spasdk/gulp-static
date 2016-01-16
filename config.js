115601f7709785157a51232d8e6bf01c706b9c59
Stanislav Kalashnik <sk@infomir.eu>
2016-01-16 15:12:12 +0200

    rework base config to extent root config

--- config.js
+++ config.js
@@ -1,26 +1,33 @@
 /**
  * HTTP static server configuration for static gulp task.
+ *
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
  */
 
 'use strict';
 
-// public
-module.exports = {
-    // turn on/off server
-    active: true,
+var extend = require('extend'),
+    config = require('spa-gulp/config');
 
-    // listening HTTP port to serve project files
-    port: 8000,
 
-    // static file server cache activation
-    // false to disable of amount of seconds to cache
-    cache: false,
+// base config
+// each profile inherits all options from the "default" profile
+module.exports = extend(true, {}, config, {
+    default: {
+        // listening HTTP port to serve project files
+        port: 8000,
 
-    // full logging
-    logging: true,
+        // static file server cache activation
+        // false to disable or amount of seconds to cache
+        cache: false,
 
-    // enable automatic reload on file changes mode
-    // set boolean value "true" to work on the default port 35729
-    // or specify some custom port value
-    livereload: true
-};
+        // full logging
+        logging: true,
+
+        // enable automatic reload on file changes mode
+        // set boolean value "true" to work on the default port 35729
+        // or specify some custom port value
+        livereload: true
+    }
+});
