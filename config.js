03a2701d850baa5fd1f1945e423c7ef9fb8b2180
Stanislav Kalashnik <sk@infomir.eu>
2016-01-26 19:54:04 +0200

    rework config handling

--- config.js
+++ config.js
@@ -7,51 +7,47 @@
 
 'use strict';
 
-var path   = require('path'),
-    extend = require('extend'),
-    config = require('spa-gulp/config'),
-    dir    = config.default.target.replace(/\\/g, '/');
+var path     = require('path'),
+    extend   = require('extend'),
+    config   = require('spa-gulp/config'),
+    profiles = {};
 
 
-// base config
-// each profile inherits all options from the "default" profile
-module.exports = extend(true, {}, config, {
-    default: {
-        // directory to serve
-        source: '.',
+// main
+profiles.default = extend(true, {}, config, {
+    // directory to serve
+    source: '.',
 
-        // main entry point to load web page
-        target: 'http://${host}:${port}/' + dir + '/index.html',
+    // main entry point to load web page
+    target: 'http://${host}:${port}/index.html',
 
-        // listening HTTP port to serve project files
-        port: 'auto',
+    // listening HTTP port to serve project files
+    port: 'auto',
 
-        // static file server cache activation
-        // false to disable or amount of seconds to cache
-        cache: false,
+    // static file server cache activation
+    // false to disable or amount of seconds to cache
+    cache: 3600,
 
-        // enable automatic reload on file changes mode
-        // set boolean value "true" to work on the default port 35729
-        // or specify some custom port value
-        livereload: true,
-
-        // info channels
-        notifications: {
-            popup: {
-                info: {
-                    icon: path.join(__dirname, 'media', 'info.png')
-                },
-                warn: {
-                    icon: path.join(__dirname, 'media', 'warn.png')
-                },
-                fail: {
-                    icon: path.join(__dirname, 'media', 'fail.png')
-                }
-            }
+    // info channels
+    notifications: {
+        popup: {
+            info: {icon: path.join(__dirname, 'media', 'info.png')},
+            warn: {icon: path.join(__dirname, 'media', 'warn.png')},
+            fail: {icon: path.join(__dirname, 'media', 'fail.png')}
         }
-    },
-
-    develop: {
-        target: 'http://${host}:${port}/' + dir + '/develop.html'
     }
 });
+
+
+profiles.develop = extend(true, {}, profiles.default, {
+    // main entry point to load web page
+    target: 'http://${host}:${port}/' + config.target.replace(/\\/g, '/') + '/develop.html',
+
+    // static file server cache activation
+    // false to disable or amount of seconds to cache
+    cache: false
+});
+
+
+// public
+module.exports = profiles;
