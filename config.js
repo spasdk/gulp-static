f674ee8b8e7430b141fb46024c0e7d282218015d
Stanislav Kalashnik <sk@infomir.eu>
2016-02-01 12:13:55 +0200

    remove connection with jade plugin

--- config.js
+++ config.js
@@ -10,17 +10,16 @@
 var path     = require('path'),
     extend   = require('extend'),
     config   = require('spa-gulp/config'),
-    jade     = require('spa-gulp-jade/config'),
     profiles = {};
 
 
 // main
 profiles.default = extend(true, {}, config, {
     // directory to serve
-    source: path.dirname(jade.default.target),
+    source: config.target,
 
     // main entry point to load web page
-    target: path.basename(jade.default.target),
+    target: 'index.html',
 
     // listening port (0 - random)
     port: 0,
@@ -45,8 +44,7 @@ profiles.develop = extend(true, {}, profiles.default, {
     source: '.',
 
     // main entry point to load web page
-    //target: config.target.replace(/\\/g, '/') + '/develop.html',
-    target: jade.develop.target.replace(/\\/g, '/'),
+    target: config.target.replace(/\\/g, '/') + '/develop.html',
 
     // static file server cache activation
     // false to disable or amount of seconds to cache
